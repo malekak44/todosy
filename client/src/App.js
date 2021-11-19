11e8aa4f8856345f2ae28194231987806ee558ac
malekak44 <malekak44@gmail.com>
2021-11-19 22:40:47 +0600

    context done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Todos from './components/Todos';
+import { useGlobalContext } from './context/AppContext';
 
 export default function App() {
+  const { login, user } = useGlobalContext();
+
   return (
     <>
       <Todos />
+      <button style={{ margin: 'auto', display: 'block' }} onClick={() => login({})}>Login</button>
+      <p style={{ textAlign: 'center' }}>{user?.name}</p>
     </>
   )
 }
--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,11 +1,46 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { url } from "../utils/url";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const fetchUser = async () => {
+        try {
+            const { data } = await axios.get(`${url}/showMe`);
+            setUser(data.user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const login = async (user) => {
+        try {
+            const { data } = await axios.post(`${url}/auth/login`, user);
+            setUser(data.user);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const logout = () => {
+        setUser(null);
+    }
+
+    useEffect(() => {
+        fetchUser();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={{
+            user,
+            login,
+            logout,
+            fetchUser,
+        }}>
             {children}
         </AppContext.Provider>
     )
