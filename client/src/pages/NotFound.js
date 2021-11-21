f47955629e414bb86d52e1a6f119c6068c8a1b26
malekak44 <malekak44@gmail.com>
2021-11-21 14:19:07 +0600

    not found done

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notfoundImg from '../assets/not-found.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>NotFound</h1>
-        </div>
+        <div className="not-found">
+            <img src={notfoundImg} alt="not found" />
+            <div className="content">
+                <h3>Ohh! Page Not Found</h3>
+                <p>We can't seem to find the page you're looking for.</p>
+                <Link to="/">Back Home</Link>
+            </div>
+        </div >
     );
 };
 
