f828cc219f555c2748b466818636812bb1a2c7f8
malekak44 <malekak44@gmail.com>
2021-11-30 09:33:25 +0600

    home header responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Header from '../components/Home/Header';
-import Contents from '../components/Home/Contents';
-import FreeUp from '../components/Home/FreeUp';
-import About from '../components/Home/About';
-import Achieve from '../components/Home/Achieve';
-import Footer from '../components/Home/Footer';
+// import Contents from '../components/Home/Contents';
+// import FreeUp from '../components/Home/FreeUp';
+// import About from '../components/Home/About';
+// import Achieve from '../components/Home/Achieve';
+// import Footer from '../components/Home/Footer';
 
 const Home = () => {
     return (
         <section className="home">
             <Header />
-            <Contents />
+            {/* <Contents />
             <FreeUp />
             <About />
             <Achieve />
-            <Footer />
+            <Footer /> */}
         </section>
     );
 };
