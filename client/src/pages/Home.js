8a59ea36ae5260a0424f8ba7a65ec89caf5b5867
malekak44 <malekak44@gmail.com>
2021-11-30 10:22:42 +0600

    home achieve responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import Header from '../components/Home/Header';
 import Contents from '../components/Home/Contents';
 import FreeUp from '../components/Home/FreeUp';
 import About from '../components/Home/About';
-// import Achieve from '../components/Home/Achieve';
+import Achieve from '../components/Home/Achieve';
 // import Footer from '../components/Home/Footer';
 
 const Home = () => {
@@ -13,8 +13,8 @@ const Home = () => {
             <Contents />
             <FreeUp />
             <About />
-            {/*<Achieve />
-            <Footer /> */}
+            <Achieve />
+            {/*<Footer /> */}
         </section>
     );
 };
